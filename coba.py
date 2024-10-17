5cef1c0cc5320d07bb16640105679b278ba53ec6
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-10-18 00:12:43 +0700

    remove unused code

--- coba.py
+++ coba.py
@@ -1,5 +1,6 @@
 import cv2
 import core
+import image_uploader
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -7,12 +8,14 @@ cap.set(4, 480)
 
 while True:
     ret, frame = cap.read()
+    orig = frame.copy()
+
     frame = cv2.flip(frame, 1)
-
     red, green, frame = core.find_balls(frame)
-
+    
     cv2.imshow("Frame", frame)
+    image_uploader.upload_image(orig, "Aku")
 
-    key = cv2.waitKey(100) & 0xFF
+    key = cv2.waitKey(10000) & 0xFF
     if key == ord("q"):
         break
