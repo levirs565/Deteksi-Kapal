1e05b0ccdc2d694612e9083cb998af59c2317651
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-10-24 05:25:27 +0700

    add gui motor tester

--- motor_controller.py
+++ motor_controller.py
@@ -1,5 +1,7 @@
 import serial
 import serial_worker
+import json
+import os.path
 
 def start():
     base_microsecond = 2200
@@ -8,24 +10,49 @@ def start():
 trim_left = 7
 trim_right = 0
 base_speed = 50
-turn_diff = 10
-turn_slow_diff = 5
+turn_left_diff = 10
+turn_right_diff = 10
+
+config_file = "./motor.json"
+
+if os.path.isfile(config_file):
+    with open(config_file, "r") as f:
+        data = json.load(f)
+        trim_left = data["trim_left"]
+        trim_right = data["trim_right"]
+        base_speed = data["base_speed"]
+        turn_left_diff = data["turn_left_diff"]
+        turn_right_diff = data["turn_right_diff"]
+
+def save_config():
+    data = {
+        "trim_left": trim_left,
+        "trim_right": trim_right,
+        "base_speed": base_speed,
+        "turn_left_diff": turn_left_diff,
+        "turn_right_diff": turn_right_diff
+    }
+    text = json.dumps(data)
+    with open(config_file, "w") as f:
+        f.write(text)
 
 last_left_speed = 0
 def set_motor_left(speed):
     global last_left_speed
-    if speed == last_left_speed: return
-    print("Changing left speed")
-    serial_worker.set_motor_left_speed(speed - (trim_left if speed >= 50 else 0))
-    last_left_speed = speed
+    new_speed = max(0, speed - trim_left)
+    if new_speed == last_left_speed: return
+    print(f"Changing left speed to {new_speed}")
+    serial_worker.set_motor_left_speed(new_speed)
+    last_left_speed = new_speed
 
 last_right_speed = 0
 def set_motor_right(speed):
     global last_right_speed
-    if speed == last_right_speed: return
-    print("Changing right speed")
-    serial_worker.set_motor_right_speed(speed - (trim_right if speed >= 50 else 0))
-    last_right_speed = speed
+    new_speed = max(0, speed - trim_right)
+    if new_speed == last_right_speed: return
+    print(f"Changing right speed to {new_speed}")
+    serial_worker.set_motor_right_speed(new_speed)
+    last_right_speed = new_speed
 
 
 # 0 = forward, -1 = kiri, 1 = kanan
@@ -35,12 +62,12 @@ def set_direction(dir):
         set_motor_right(base_speed)
         print("Move forward")
     elif dir < 0:
-        set_motor_left(base_speed - turn_diff)
+        set_motor_left(base_speed - turn_left_diff)
         set_motor_right(base_speed)
         print("Turn left")
     elif dir > 0:
         set_motor_left(base_speed)
-        set_motor_right(base_speed - turn_diff)
+        set_motor_right(base_speed - turn_right_diff)
         print("Turn right")
 
 def stop():
---
+++ motor_tester_gui.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+import serial_worker
+import motor_controller
+
+window = tk.Tk()
+
+def createTrackbarFrame(text, value, minimum, maximum, onChanged):
+    frame = tk.Frame(window, padx=16, pady=16)
+    label = tk.Label(frame, text=text, width=20)
+    label.pack(side=tk.LEFT)
+
+    valueLabel = tk.Label(frame, text=str(value), width=4)
+    valueLabel.pack(side=tk.LEFT)
+
+    def trackChanged(x):
+        valueLabel.config(text=str(trackbar.get()))
+        onChanged(trackbar.get())
+
+    def decrement():
+        trackbar.set(max(trackbar.get() - 1, minimum))
+
+    def increment():
+        trackbar.set(min(maximum, trackbar.get() + 1))
+
+    decButton = tk.Button(frame, text="-", command=decrement)
+    decButton.pack(side=tk.LEFT)
+    trackbar = tk.Scale(frame, from_=minimum, to=maximum, orient=tk.HORIZONTAL, showvalue=False, command=trackChanged)
+    trackbar.set(value)
+    trackbar.pack(side=tk.LEFT,fill=tk.X,expand=True)
+    incButton = tk.Button(frame, text="+", command=increment)
+    incButton.pack(side=tk.LEFT, fill=tk.X)
+    frame.pack(fill=tk.X)
+
+enabledVar = tk.BooleanVar()
+currentDirection = 0
+
+def onEnableChanged():
+    global enabled
+    enabled = enabledVar.get()
+    if enabled:
+        motor_controller.set_direction(currentDirection)
+    else:
+        motor_controller.stop()
+
+def onDirectionChanged(x):
+    global currentDirection
+    currentDirection = x
+    if enabledVar.get():
+        motor_controller.set_direction(x)
+
+def updateSpeed():
+    if enabledVar.get():
+        motor_controller.set_direction(currentDirection)
+
+def onBaseSpeedChanged(x):
+    motor_controller.base_speed = x
+    updateSpeed()
+    motor_controller.save_config()
+
+def onLeftTrimChanged(x):
+    motor_controller.trim_left = x
+    updateSpeed()
+    motor_controller.save_config()
+
+def onRightTrimChanged(x):
+    motor_controller.trim_right = x
+    updateSpeed()
+    motor_controller.save_config()
+
+def onTurnLeftDiffChanged(x):
+    motor_controller.turn_left_diff = x
+    updateSpeed()
+    motor_controller.save_config()
+
+def onTurnRightDiffChanged(x):
+    motor_controller.turn_right_diff = x
+    updateSpeed()
+    motor_controller.save_config()
+
+check = tk.Checkbutton(window, text="Enable Motor", variable=enabledVar, command=onEnableChanged)
+check.pack()
+
+createTrackbarFrame("Direction", 0, -1, 1, onDirectionChanged)
+createTrackbarFrame("Base Speed", motor_controller.base_speed, 0, 100, onBaseSpeedChanged)
+createTrackbarFrame("Left Trim", motor_controller.trim_left, 0, 50, onLeftTrimChanged)
+createTrackbarFrame("Right Trim", motor_controller.trim_right, 0, 50, onRightTrimChanged)
+createTrackbarFrame("Turn Left, Left Diff", motor_controller.turn_left_diff, 0, 50, onTurnLeftDiffChanged)
+createTrackbarFrame("Turn Right, Right Diff", motor_controller.turn_right_diff, 0, 50, onTurnRightDiffChanged)
+
+tk.mainloop()
